acc5432b9a1ac2aaf5ada0edbd4e0e40d0a2e460
Patience Guisset <patience.guisset@gmail.com>
2018-11-20 17:48:01 +0100

    Wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,15 @@
+require 'faker'
+
+puts 'Creating 10 fake articles...'
+10.times do
+  article = Article.new(
+    title: Faker::Music.album,
+    content: Faker::Music.instrument
+    # votes:  rand(0..5)
+  )
+  article.save!
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
